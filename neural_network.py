9650e191d2008ed4ccf79c5949183b7797ce8eb8
Diogo Soares <diogo.soares05@gmail.com>
2023-10-11 23:49:28 +0200

    Commiting from nvim

--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,12 @@
-import numpy as np
-
-inputs = [1, 2, 3, 2.5]
-
-
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2, 3, 0.5]
-
-output = np.dot(weights, inputs) + biases
-print(output)
-
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+#
+    def forward(self, input):
+        # TODO: return output
+        pass
+#
+    def backward(self, output_gradient, learning_rate):
+        # TODO: update parameters and return input input gradient
+        pass
